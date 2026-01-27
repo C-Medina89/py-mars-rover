241b233766a6e9ac9731efa261f3a878b39e790c
C-Medina89 <m.masud@lbhospital.med.ly>
2026-01-27 16:42:57 +0000

    feat: Add basic rover tracking to plateau

--- logic_layer/plateau.py
+++ logic_layer/plateau.py
@@ -10,4 +10,9 @@ class Plateau:
     def is_rover_within_bounds(self, x: int, y: int)-> bool:
         return 0 <= x <= self.width and 0 <= y <= self.height
     
-   
+    def add_rover(self, rover):
+    # add rover to the plateau
+    self.rovers.append(rover) 
+
+    def get_rovers(self):
+        return self.rovers
